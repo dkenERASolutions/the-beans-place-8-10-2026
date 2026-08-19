0105e59019e94cd14b0e22e6d71131370f8b90de
Danny Ken <d.ken@erasolutions.us>
2026-08-19 09:03:40 +0800

    finished productshowcase.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -58,6 +58,7 @@ import FooterSection from "./components/FooterSection";
 import NavBar from "./components/NavBar";
 import RibbonTicker from "./components/RibbonTicker";
 import FeaturesSection from "./components/FeaturesSection";
+import ProductShowCase from "./components/ProductShowcase";
 
 export default function App() {
     return (
@@ -80,7 +81,9 @@ export default function App() {
             </section>
 
             {/* PRODUCT SHOWCASE */}
-            <section className="bg-cta"></section>
+            <section className="bg-cta">
+                <ProductShowCase />
+            </section>
 
             {/* CTA */}
             <section className="bg-cta"></section>
