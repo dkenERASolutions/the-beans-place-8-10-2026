9a6d5e29217cf33e17f628df97b60c82e571905d
Danny Ken <d.ken@erasolutions.us>
2026-08-20 08:36:40 +0800

    worked on aboutsection.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -59,6 +59,7 @@ import NavBar from "./components/NavBar";
 import RibbonTicker from "./components/RibbonTicker";
 import FeaturesSection from "./components/FeaturesSection";
 import ProductShowCase from "./components/ProductShowcase";
+import AboutSection from "./components/AboutSection";
 
 export default function App() {
     return (
@@ -89,7 +90,9 @@ export default function App() {
             <section className="bg-cta"></section>
 
             {/* ABOUT */}
-            <section className="bg-cta" id="about"></section>
+            <section className="bg-cta" id="about">
+                <AboutSection />
+            </section>
 
             {/* CONTACT */}
             <section className="bg-cta" id="contact"></section>
